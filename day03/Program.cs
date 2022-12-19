22a12d065a630306e2c3cb3245e7895008995ff3
Roger Nyberg <glideroggan@gmail.com>
2022-12-19 09:48:36 +0100

    day 03 - part1

---
+++ day03/Program.cs
@@ -0,0 +1,24 @@
+﻿var rawInput = await ReadInput();
+
+async Task<string[]> ReadInput()
+{
+    return await File.ReadAllLinesAsync("training.txt");
+}
+
+class Helper
+{
+    void Parse(string[] rawInput)
+    {
+        foreach (var line in rawInput)
+        {
+            var span = line.AsSpan();
+            var comp1 = span[..(line.Length/2)];
+            var comp2 = span[(line.Length/2)..];
+            // we need to filter out those items that are in BOTH compartments
+            
+            
+        }        
+    }
+}
+
+
